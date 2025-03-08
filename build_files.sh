f82072f8b7a6836d0d33082736aaf96afea58b71
sainivas-yellapragada <sainivasyellapragada@gmail.com>
2025-03-08 15:00:27 +0530

    updated buildsh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,17 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+#!/bin/bash
+
+echo "🔹 Creating a virtual environment..."
+python3 -m venv venv  
+source venv/bin/activate  
+
+echo "🔹 Upgrading pip..."
+pip install --upgrade pip  
+
+echo "🔹 Installing dependencies..."
+pip install -r requirements.txt  
+
+echo "🔹 Checking installed packages..."
+pip freeze  
+
+echo "🔹 Collecting static files..."
+python3 manage.py collectstatic --noinput  
